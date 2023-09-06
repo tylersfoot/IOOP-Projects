1678903c9fffa3a39c91551d67c94f526aa27063
tylersfoot <tylerdepa8@gmail.com>
2023-09-06 12:59:53 -0400

    lab1

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * A program to display a message to the screen.
+ *
+ * @author (Tyler D)
+ * @version (v1.0, 9/6/23)
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
